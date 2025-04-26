9e891d75771d58b33a8ef0a32fafa67223970521
haohao.brian <brian.chunhao@gmail.com>
2025-04-26 17:29:03 +0800

    Update 1-4.c

--- unix/1-4.c
+++ unix/1-4.c
@@ -6,8 +6,8 @@ int main(void)
   char buf[BUFFSIZE];
   while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
     if (write(STDOUT_FILENO, buf, n) != n)
-      err_sys("write error");
+      //err_sys("write error");
   if (n < 0)
-    err_sys("read error");
+    //err_sys("read error");
   exit(0);
 }
